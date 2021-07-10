e58399b6856e309d64cca3a5127dfbbdfa60db5e
pioday <pioday98@gmail.com>
2021-07-10 14:59:48 +0900

    run commit

---
+++ pioday_SSR_0710/routes/main.js
@@ -0,0 +1,96 @@
+const express = require('express')  //라우팅 모듈
+const app = express()
+const port = 3000
+
+const fs = require('fs')    // 파일 리드 모듈
+const mysql = require('mysql2');    // mysql 모듈
+const crypto = require('crypto');  // 암호화 모듈
+
+const conn = mysql.createConnection({   // localhost로 만든 db와 connection 연결
+    host : 'localhost',
+    user : 'root',
+    password : '1234',
+    database : 'test'
+})
+
+let rf = fs.readFileSync('./my_data/signup.html', 'utf-8', (err, data) => data)
+
+
+app.get('/', (req, res) =>{     // root 경로에 rf파일 전송
+    res.send(rf)
+})
+
+
+let count = 0;
+
+app.use( (req,res,next) => {        // 접속한 사용자를 파악하기 위한 미들웨어
+    count++;
+    let today = new Date();
+    console.log(count +' 번째 사용자 접속 시간 : ' + today);
+    next();
+})
+
+
+app.get('/Signup', (req, res) =>{       //req. 요청. client -> server
+
+        var id = req.query.id;
+        var pw = req.query.pw;
+
+        //crypeto.createHash('sha512').update('123').digest('base64');
+
+        var AESpw = crypto.createHash('sha512').update(pw).digest('base64');
+        var arr = [id,AESpw];
+
+        var I_sql = 'insert into user(id, pw) values(?,?)';
+
+        conn.query(I_sql,arr, (err,results,fields) =>{
+            if(err){
+                console.log(err)
+                res.send("가입 실패")
+            }
+            else{
+                res.send("id : "+ arr[0] +'\n 가입 성공!')
+            }
+    });
+});
+
+
+app.get('/Login', (req, res) =>{
+
+        var id = req.query.id;
+        var pw = req.query.pw;
+
+        var AESpw = crypto.createHash('sha512').update(pw).digest('base64');
+
+        var arr = [id,AESpw];
+
+        var S_sql = 'select id , pw , case when id = ? and pw = ? then true else false END as val from user';
+
+        conn.query(S_sql,arr, (error,results,fields) =>{
+            if(error){
+                console.log(error)
+            }
+            else{
+                let is_exist = false;
+                let i;
+
+                for (i = 0 ; i < results.length ; i++){
+                    if(results[i].val == true)     // true로 일치하는 계정이 있다면
+                        is_exist = true;        // 존재한다.
+                }
+
+                if(is_exist){
+                    let lo = fs.readFileSync('./my_data/Login.html', 'utf-8', (err, data) => data)
+                    res.send(lo)
+                }
+                else{
+                    res.send("실패")
+                }                
+            }
+        });
+    })
+
+
+app.listen(port, () =>{
+    console.log(`Example app listening at http://localhost:${port}`)
+})
